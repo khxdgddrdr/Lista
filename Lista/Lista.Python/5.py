f5c5b18ba029650557d81559f08e9bbc19e42d28
khxdgddrdr <125392474+khxdgddrdr@users.noreply.github.com>
2023-04-11 09:39:47 -0300

    Update 5.py

--- Lista/Lista.Python/5.py
+++ Lista/Lista.Python/5.py
@@ -1,7 +1,7 @@
-string1 = input("Digite a primeira string: ")
-string2 = input("Digite a segunda string: ")
+stri1 = input("Digite a primeira string: ")
+stri2 = input("Digite a segunda string: ")
 
-if string1 == string2:
+if stri1 == stri2:
     print("As duas strings são iguais.")
 else:
     print("As duas strings são diferentes.")
