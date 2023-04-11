7dd2d3e41014d5114bd99ab53fdebc6650a5dc1a
khxdgddrdr <125392474+khxdgddrdr@users.noreply.github.com>
2023-04-11 09:39:20 -0300

    Update 4.py

--- Lista/Lista.Python/4.py
+++ Lista/Lista.Python/4.py
@@ -1,3 +1,3 @@
-letra_maiuscula = input("Digite uma letra maiúscula: ")
-letra_minuscula = chr(ord(letra_maiuscula) + 32)
-print(f"A letra minúscula correspondente é {letra_minuscula}")
+l_maiuscula = input("Digite uma letra maiúscula: ")
+l_minuscula = chr(ord(l_maiuscula) + 32)
+print(f"A letra minúscula correspondente é {l_minuscula}")
