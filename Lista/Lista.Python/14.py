72334619dda40d1ae1e956914769acd1a70e4ebe
khxdgddrdr <125392474+khxdgddrdr@users.noreply.github.com>
2023-04-11 09:32:49 -0300

    Update 14.py

--- Lista/Lista.Python/14.py
+++ Lista/Lista.Python/14.py
@@ -1,19 +1,19 @@
-palavra = input("Digite uma palavra: ")
-vogais = 'aeiou'
-num_vogais = 0
+p = input("Digite uma palavra: ")
+vo = 'aeiou'
+n_vogais = 0
 
-for letra in palavra:
-    if letra.lower() in vogais:
-        num_vogais += 1
+for letra in p:
+    if letra.lower() in vo:
+        n_vogais += 1
 
 substituir = input("Digite um caractere para substituir as vogais: ")
 
 nova_palavra = ""
-for letra in palavra:
-    if letra.lower() in vogais:
+for letra in p:
+    if letra.lower() in vo:
         nova_palavra += substituir
     else:
         nova_palavra += letra
 
-print("A palavra", palavra, "possui", num_vogais, "vogais.")
+print("A palavra", p, "possui", n_vogais, "vogais.")
 print("Nova palavra:", nova_palavra)
