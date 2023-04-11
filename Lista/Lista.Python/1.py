5b49e86daf1d1c5ac326500a7e5d343854dfb03b
khxdgddrdr <125392474+khxdgddrdr@users.noreply.github.com>
2023-04-11 09:24:35 -0300

    Update 1.py

--- Lista/Lista.Python/1.py
+++ Lista/Lista.Python/1.py
@@ -1,9 +1,9 @@
-palavra = input("Digite uma palavra de até 10 caracteres: ")
-vogais = "aeiouAEIOU" # Definindo as vogais
+pla = input("Digite uma palavra de até 10 caracteres: ")
+vos = "aeiouAEIOU"
 
-num_vogais = 0
+n_vogais = 0
 for letra in palavra:
     if letra in vogais:
-        num_vogais += 1
+        n_vogais += 1
 
 print("A palavra", palavra, "possui", num_vogais, "vogais.")
