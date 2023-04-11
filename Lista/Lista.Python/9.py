a4f11d017abdb11708800fbcd3fb8ec0f101265f
khxdgddrdr <125392474+khxdgddrdr@users.noreply.github.com>
2023-04-11 09:41:53 -0300

    Update 9.py

--- Lista/Lista.Python/9.py
+++ Lista/Lista.Python/9.py
@@ -1,8 +1,8 @@
-string = input("Digite a string: ")
-count = 0
+stri = input("Digite a string: ")
+cou = 0
 
-for char in string:
-    if char == '1':
-        count += 1
+for cha in stri:
+    if cha == '1':
+        cou += 1
 
-print("O número de ocorrências do caractere '1' na string é:", count)
+print("O número de ocorrências do caractere '1' na string é:", cou)
